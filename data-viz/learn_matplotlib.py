f92009777e6b0bbcb74fba39f3c3ccde9a7d265d
Philip Martins <philip.martt@outlook.com>
2024-06-18 22:18:10 -0300

    feat: added a bar chart

--- data-viz/learn_matplotlib.py
+++ data-viz/learn_matplotlib.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 
+# line chart
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
 
@@ -12,3 +13,12 @@ plt.ylabel("Billions of $")
 plt.savefig('nominal_gdp')
 
 plt.show()
+
+# bar chart
+plt.bar(years, gdp)
+
+plt.ylabel("Billions of $")
+
+plt.show()
+
+plt.savefig('nominal_gdp_bar')
