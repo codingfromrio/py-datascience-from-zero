85fade075060a0364d12d8ec5eaefa4938e679d8
Philip Martins <philip.martt@outlook.com>
2024-06-28 09:45:43 -0300

    feat: vectors

---
+++ linear-algebra/vectors.py
@@ -0,0 +1,19 @@
+from typing import List
+
+# vetor tridimensional
+Vector = List[float]
+
+# vetor com o altura, peso e idade
+height_weight_age = [
+    70, # polegadas
+    170, # libras
+    40, # anos
+]
+
+# vetor com as notas de provas
+provas = [
+    95, # prova 1
+    80, # prova 2
+    75, # prova 3
+    62, # prova 4
+]
